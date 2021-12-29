1124e013c433fa928d85eb0b9c6f90bd64dee283
modul8co <66965855+modul8co@users.noreply.github.com>
2021-12-29 17:17:15 +0100

    Implement platform

--- src/platform.ts
+++ src/platform.ts
@@ -1,116 +1,80 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
-
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { API, Logger, PlatformConfig, Service, Characteristic} from 'homebridge';
+import { Parser } from 'xml2js';
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class HikvisionNVREventsPlatform {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
-
-  // this is used to track restored cached accessories
-  public readonly accessories: PlatformAccessory[] = [];
+  private _urllib = require('urllib');
+  private _requestOptions: { streaming: boolean; digestAuth: string };
+  private _streamingOptions: { streaming: boolean; digestAuth: string };
+  private _parser = new Parser({ explicitArray: false });
+  private _channelNames = {};
+  private _eventsUrl: string;
+  private _channelsUrl: string;
+  private _channelRegex = new RegExp('(?<=<dynChannelID>)\\d+');
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
-
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
+    this._requestOptions = {
+      streaming: false,
+      digestAuth: `${config.username}:${config.password}`,
+    };
+    this._streamingOptions = {
+      streaming: true,
+      digestAuth: `${config.username}:${config.password}`,
+    };
+    this._eventsUrl = `http://${config.host}:${config.port}/ISAPI/Event/notification/alertStream`;
+    this._channelsUrl = `http://${config.host}:${config.port}/ISAPI/ContentMgmt/InputProxy/channels`;
     this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      this.startListening();
     });
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
-  configureAccessory(accessory: PlatformAccessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
-
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
+  async startListening() {
+    await this._urllib.request(this._channelsUrl, this._requestOptions).then((result) => {
+      this._parser.parseString(result.data.toString(), (err, result)=> {
+        result.InputProxyChannelList.InputProxyChannel.forEach((channel) => {
+          this._channelNames[channel.id] = channel.name;
+          this.log.debug(`Added camera ${channel.name} on channel ${channel.id}`);
+        });
+      });
+      this._urllib.request(this._eventsUrl, this._streamingOptions, this.responseHandler);
+    });
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
+  async motionUpdater(channel: string){
+    this.log.debug('Enabling motion for', channel);
+    await this._urllib.request(`${this.config.motionurl}?${channel}`).catch((err) => {
+      this.log.error('Could not update motion for channel', channel, err);
+    });
+  }
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
-
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
+  responseHandler = (err, data, res) => {
+    if (err) {
+      this.log.error('Error:', err);
     }
-  }
+    if (res !== null) {
+      this.log.debug('Listening for motion events on:', this._eventsUrl);
+      res.on('data', (data) => {
+        const channelMatches = data.toString().match(this._channelRegex);
+        if (channelMatches !== null) {
+          channelMatches.forEach((channelId) => {
+            this.motionUpdater(this._channelNames[channelId]);
+          });
+        }
+      });
+      res.on('error', (err) => {
+        this.log.error('Error', err);
+      });
+    }
+  };
+
 }
