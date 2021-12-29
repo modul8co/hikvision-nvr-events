daf29c5714c0dba28152eebec275e5390aacf33a
modul8co <66965855+modul8co@users.noreply.github.com>
2021-12-29 17:21:33 +0100

    Rename plugin

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'Hikvision NVR Events';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-hikvision-nvr-events';
+export const PLUGIN_NAME = '@modul8co/homebridge-hikvision-nvr-events';
